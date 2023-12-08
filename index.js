06117899a8b8c62a039bbea18f653a7aba3dc272
aziza <azizafazliddinova08@gmail.com>
2023-12-08 22:15:21 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+let box = document.querySelector('#num')
+
+let btn1 = document.querySelector('.dec')
+
+let btn2 = document.querySelector('.reset')
+
+let btn3 = document.querySelector('.inc')
+
+let btn4 = document.querySelector('.random')
+
+let a = 0
+
+
+btn1.addEventListener("click", () => {
+    if (a > 0) {
+        a--;
+        box.textContent = a
+    }
+})
+
+btn2.addEventListener("click", () => {
+    a = 0
+    box.textContent = a
+})
+
+btn3.addEventListener("click", () => {
+    if (a < 10000) {
+        a++
+        box.textContent = a
+    }
+})
+
+btn4.addEventListener("click", () => {
+    a = Math.floor(Math.random() * 10000) + 1
+    box.textContent = a
+})
